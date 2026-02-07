504281bb34e2da2d30b1a5e1fa1b40b8fb04ee37
antonio-decaro <antonio.decaro99@outlook.it>
2026-02-07 16:05:02 +0100

    addedd shortcuts for fugitive

--- init.lua
+++ init.lua
@@ -286,7 +286,14 @@ require('lazy').setup({
     },
   },
 
-  { 'tpope/vim-fugitive' }, -- Add support for Git management
+  {
+    'tpope/vim-fugitive',
+    keys = {
+      { '<leader>gg', '<cmd>Git<cr>', desc = 'Fugitive: Git' },
+      { '<leader>gp', '<cmd>Git push<cr>', desc = 'fugitive: git push' },
+      { '<leader>gP', '<cmd>Git pull<cr>', desc = 'fugitive: git pull' },
+    },
+  }, -- Add support for Git management
 
   {
     'akinsho/toggleterm.nvim',
