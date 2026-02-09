b8f5154f730a5321c1657344248a8ea8ce118878
antonio-decaro <antonio.decaro99@outlook.it>
2026-02-09 19:59:44 +0100

    added script for ui customization

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        -- config
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+  { -- Display a nice notification when you save a file
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+}
