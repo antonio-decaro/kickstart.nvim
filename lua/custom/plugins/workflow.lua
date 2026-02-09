1106503d8aa6369cbd5d26eb74009ed552c7b494
antonio-decaro <antonio.decaro99@outlook.it>
2026-02-09 13:27:42 +0100

    add cmake workflow

---
+++ lua/custom/plugins/workflow.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'Civitasv/cmake-tools.nvim',
+    opts = {},
+    keys = {
+      { '<leader>mb', '<cmd>CMakeBuild -j<cr>', mode = { 'n' }, desc = 'C[M]ake [B]uild' },
+      { '<leader>md', '<cmd>CMakeSelectBuildDir <cr>', mode = { 'n' }, desc = 'C[M]ake Select Build [D]ir' },
+      { '<leader>mt', '<cmd>CMakeSelectBuildTarget <cr>', mode = { 'n' }, desc = 'C[M]ake Select Build [T]arget' },
+      { '<leader>mD', '<cmd>CMakeSelectCwd <cr>', mode = { 'n' }, desc = 'C[M]ake Select current working [D]irectory' },
+      { '<leader>mc', '<cmd>CMakeOpenCache <cr>', mode = { 'n' }, desc = 'C[M]ake Open [C]ache' },
+    },
+  },
+}
