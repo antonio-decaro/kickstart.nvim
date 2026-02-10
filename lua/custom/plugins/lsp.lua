3e3cbe41cfa52b88dbe846efa97edd76f6b0cca0
antonio-decaro <antonio.decaro99@outlook.it>
2026-02-10 02:03:23 +0100

    fixed lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'mason-org/mason.nvim', opts = {} },
+      'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       { 'j-hui/fidget.nvim', opts = {} },
       'saghen/blink.cmp',
@@ -68,33 +69,43 @@ return {
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      for name, server in pairs(servers) do
+      local mason_lspconfig = require 'mason-lspconfig'
+      mason_lspconfig.setup { automatic_enable = false }
+
+      local function setup_server(server_name)
+        local server = servers[server_name] or {}
+
+        if server_name == 'lua_ls' then
+          server = vim.tbl_deep_extend('force', {}, server, {
+            on_init = function(client)
+              if client.workspace_folders then
+                local path = client.workspace_folders[1].name
+                if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+              end
+
+              client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                  version = 'LuaJIT',
+                  path = { 'lua/?.lua', 'lua/?/init.lua' },
+                },
+                workspace = {
+                  checkThirdParty = false,
+                  library = vim.api.nvim_get_runtime_file('', true),
+                },
+              })
+            end,
+            settings = { Lua = {} },
+          })
+        end
+
         server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-        vim.lsp.config(name, server)
-        vim.lsp.enable(name)
+        vim.lsp.config(server_name, server)
+        vim.lsp.enable(server_name)
       end
 
-      vim.lsp.config('lua_ls', {
-        on_init = function(client)
-          if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-          end
-
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-              version = 'LuaJIT',
-              path = { 'lua/?.lua', 'lua/?/init.lua' },
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = vim.api.nvim_get_runtime_file('', true),
-            },
-          })
-        end,
-        settings = { Lua = {} },
-      })
-      vim.lsp.enable 'lua_ls'
+      for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
+        setup_server(server_name)
+      end
     end,
   },
 }
