8b8d9dbc60f30b24331ae2aaaff8eed859eab487
antonio-decaro <antonio.decaro99@outlook.it>
2026-02-22 19:44:39 +0100

    modified 'cmake' plugin auto enable on CMake projects

--- lua/custom/plugins/workflow.lua
+++ lua/custom/plugins/workflow.lua
@@ -1,6 +1,24 @@
 return {
   { -- CMake Tools
     'Civitasv/cmake-tools.nvim',
+    name = 'cmake-tools.nvim',
+    init = function()
+      local function has_cmake_lists(cwd)
+        return cwd and vim.fn.filereadable(cwd .. '/CMakeLists.txt') == 1
+      end
+
+      local function load_if_cmake_project()
+        if has_cmake_lists(vim.uv.cwd()) then
+          require('lazy').load { plugins = { 'cmake-tools.nvim' } }
+        end
+      end
+
+      local group = vim.api.nvim_create_augroup('CMakeToolsAutoLoad', { clear = true })
+      vim.api.nvim_create_autocmd({ 'VimEnter', 'DirChanged' }, {
+        group = group,
+        callback = load_if_cmake_project,
+      })
+    end,
     opts = {},
     keys = {
       { '<leader>mb', '<cmd>CMakeBuild -j<cr>', mode = { 'n' }, desc = 'C[M]ake [B]uild' },
