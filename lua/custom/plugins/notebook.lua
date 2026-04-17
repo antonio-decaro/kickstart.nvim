4fcca99a94f8034f19cf7c6c732bb2395fbfad10
antonio-decaro <antonio.decaro99@outlook.it>
2026-04-17 23:28:43 +0200

    commented out plugin for notebook

--- lua/custom/plugins/notebook.lua
+++ lua/custom/plugins/notebook.lua
@@ -1,17 +1,17 @@
 -- This is used to enable jupyter notebook editing in NVIM
 return {
-  {
-    'jeryldev/pyworks.nvim',
-    dependencies = {
-      'benlubas/molten-nvim',
-      '3rd/image.nvim',
-    },
-    config = function()
-      require('pyworks').setup {
-        -- Image rendering
-      } -- See Configuration section for options
-    end,
-    lazy = false,
-    priority = 100,
-  },
+  -- {
+  --   'jeryldev/pyworks.nvim',
+  --   dependencies = {
+  --     'benlubas/molten-nvim',
+  --     '3rd/image.nvim',
+  --   },
+  --   config = function()
+  --     require('pyworks').setup {
+  --       -- Image rendering
+  --     } -- See Configuration section for options
+  --   end,
+  --   lazy = false,
+  --   priority = 100,
+  -- },
 }
